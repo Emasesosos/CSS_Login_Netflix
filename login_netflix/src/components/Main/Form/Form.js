4ac6cdcb8da4d69a729e18eea2578e247f772915
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-03-26 17:24:52 -0600

    Validación del Formulario: Hooks => useState y eventos, Agregando clases y mensajes de error, Agregando botón para mostrar/ocultar la contraseña

--- login_netflix/src/components/Main/Form/Form.js
+++ login_netflix/src/components/Main/Form/Form.js
@@ -1,17 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+
+    // State para iniciar sesión
+    const [login, validarLogin] = useState({
+        usuario: '',
+        password: '',
+        estatusUsuario: false,
+        estatusPassword: false,
+        claseInputUs: '',
+        claseInputPs: '',
+        claseLabelUs: '',
+        claseLabelPs: '',
+        mensaje:'',
+        blur: false,
+        tipoPassword: 'password',
+        claseSpan: '',
+        text: 'Mostrar'
+    });
+
+    // Extraer de Usuario
+    const { usuario, password } = login;
+
+    const onChange = (e) => {
+        validarLogin({
+            ...login,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const onBlur = (e) => {
+        e.preventDefault();
+        console.log('onBlur Input!');
+        validarLogin ({
+            ...login,
+            [e.target.name]: e.target.value,
+        })
+        const input = e.target.name;
+        console.log(input);
+        // *** Validar que no haya campos vacíos *** //
+        if(usuario.trim() === '' && input === 'usuario') {
+            validarLogin({
+                ...login,
+                estatusUsuario: true,
+                claseInputUs: 'no-valido',
+                claseLabelUs: 'valido',
+                mensaje: 'Este campo es obligatorio',
+                blur: true
+            })
+        }   else if(password.trim() === '' && input === 'password') {
+                validarLogin({
+                    ...login,
+                    estatusPassword: true,
+                    claseInputPs: 'no-valido',
+                    claseLabelPs: 'valido',
+                    mensaje: 'Este campo es obligatorio',
+                    blur: true
+                })
+            } 
+    }
+
+    const onClick = (e) => {
+        console.log('span Mostrar!');
+        const clase = e.target.className;
+        console.log(clase);
+        if (clase === 'mostrar') {
+            validarLogin({
+                ...login,
+                claseSpan: '',
+                tipoPassword: 'password',
+                text: 'Mostrar'
+            })
+        } else {
+            validarLogin({
+                ...login,
+                claseSpan: 'mostrar',
+                tipoPassword: 'text',
+                text: 'Ocultar'
+            })
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('Usuario: ', login.usuario);
+        console.log('Contraseña: ', login.password);
+    }
+
     return (
-        <form action="#">
+
+        <form
+            onSubmit={onSubmit}
+        >
             <legend>Inicia Sesión</legend>
             <div className="campo">
-                <input type="text" id="usuario"/>
-                <label htmlFor="">Email o Número de Teléfono</label>
+                <input 
+                    type="text" 
+                    id="usuario"
+                    name="usuario"
+                    value={usuario}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    className={`${login.claseInputUs}`}
+                />
+                <label className={`${login.claseLabelUs}`} htmlFor="">Email o Número de Teléfono</label>
             </div>
+            { login.estatusUsuario ? (<div className="alerta">{login.mensaje}</div>) : null }
             <div className="campo">
-                <input type="password" id="password"/>
-                <label htmlFor="">Contraseña</label>
+                <input 
+                    type={login.tipoPassword} 
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    className={`${login.claseInputPs}`}
+                />
+                <label className={`${login.claseLabelPs}`} htmlFor="">Contraseña</label>
+                <span
+                    className={login.claseSpan}
+                    onClick={onClick}
+                >
+                    {login.text}
+                </span>
             </div>
+            { login.estatusPassword ? (<div className="alerta">{login.mensaje}</div>) : null }
             <div className="submit">
                 <input type="submit" value="Iniciar Sesión"/>
             </div>
@@ -25,6 +138,7 @@ const Form = () => {
                 </div>
             </div>
         </form>
+
     );
 }
 
